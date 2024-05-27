f38846104b39786fb265bf123854011f2d708413
Shadrack <skmutai6@gmail.com>
2024-05-27 07:57:04 +0300

    lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,35 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return[spicy_food["name"] for spicy_food in spicy_foods]
+print(get_names(spicy_foods))
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [spiciest_food for spiciest_food in spicy_foods if spiciest_food["heat_level"] > 5]
+print(get_spiciest_foods(spicy_foods))
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for print_spicy in spicy_foods:
+        print(f"{print_spicy['name']} ({print_spicy['cuisine']}) | Heat Level: { print_spicy['heat_level'] * '🌶' }")
+print(print_spicy_foods(spicy_foods))
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food['cuisine'] == cuisine:
+            return food
+    return None
+print(get_spicy_food_by_cuisine(spicy_foods, "Sichuan"))
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food['heat_level'] > 5:
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: { food['heat_level'] * '🌶' }")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat = sum(food['heat_level'] for food in spicy_foods)
+    return total_heat // len(spicy_foods)
+print(get_average_heat_level(spicy_foods))
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
